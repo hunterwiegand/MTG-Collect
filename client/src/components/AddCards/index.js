c6b940faf9001c85bc6164ac32c2bace96a7612f
Hunter Wiegand <hunter_wiegand@yahoo.com>
2020-03-19 19:41:24 -0700

    added form to add cards to collection

--- client/src/components/AddCards/index.js
+++ client/src/components/AddCards/index.js
@@ -1,16 +1,48 @@
 import React from "react";
 
-function AddCards({ img }) {
+function AddCards({
+    imageUrl,
+    quantity,
+    handleInputChangeAdd,
+    handleAddSubmit
+}) {
     return (
-        <div>
-            {/* Image tag to show card image, this src url will be imported from when the user searched the card */}
-            <img src={img}></img>
+        // <div>
+        //     {/* Image tag to show card image, this src url will be imported from when the user searched the card */}
+        //     <img src={imageUrl}></img>
 
-            {/* Add cards will add the card info into the users database fro collection */}
-            <button>Add Card(s)</button>
-            {/* Drop down menu for up to 6 cards */}
-            {/* text box to enter custom amount >6 */}
-        </div>
+        //     {/* Add cards will add the card info into the users database fro collection */}
+        //     <button>Add Card(s)</button>
+        //     {/* Drop down menu for up to 6 cards */}
+        //     {/* text box to enter custom amount >6 */}
+        // </div>
+
+        <form>
+            <div className="form-group">
+
+                {/* Image tag to show card image, this src url will be imported from when the user searched the card */}
+                <img src={imageUrl}></img>
+
+                <input
+                    id="quantity"
+                    type="number"
+                    value={quantity}
+                    min="0"
+                    name="quantity"
+                    onChange={handleInputChangeAdd}
+                    required
+                />
+            </div>
+            <div className="pull-right">
+                <button
+                    onClick={handleAddSubmit}
+                    type="button"
+                    className="btn btn-lg btn-danger float-right"
+                >
+                    Add Card
+  </button>
+            </div>
+        </form>
     );
 }
 
--- client/src/pages/add-cards.js
+++ client/src/pages/add-cards.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import { Col, Row, Container } from "../components/Grid";
+import { Container } from "../components/Grid";
 import Form from "../components/Form"
+import AddCards from "../components/AddCards"
 import axios from 'axios';
 
 class Add_Cards extends Component {
@@ -40,18 +41,24 @@ class Add_Cards extends Component {
                     oracle_text: data.oracle_text,
                     type: data.type,
                     cmc: data.cmc,
-                    imageUrl: data.image_uris.small
+                    imageUrl: data.image_uris.normal
                 })
             })
             // If there is an error while handling the request set the state message to inform the user to try again
             .catch(err =>
                 console.log("No cards found"));
-            this.setState({
+        this.setState({
             message: "No card was found, try again"
         });
 
 
     };
+
+    // Function to add card to collection
+    addCard = () => {
+        console.log("name: ", this.state.name);
+        console.log("amount: ", this.state.quantity);
+    }
 
     // Function for when the user submits the form
     handleFormSubmit = event => {
@@ -83,6 +90,39 @@ class Add_Cards extends Component {
         });
     };
 
+    // Function for when the user submits the Add button
+    handleAddSubmit = event => {
+        event.preventDefault();
+       
+        // Run function for find cards using the API
+        this.addCard();
+
+        // Empty out the current state for the new search
+        this.setState({
+            quantity: "",
+            colors: [],
+            mana_cost: "",
+            name: "",
+            type_line: "",
+            rarity: "",
+            oracle_text: "",
+            cmc: "",
+            imageUrl: "",
+            message: "Search another card!"
+        })
+    };
+
+    // function used to make changes to the state
+    handleInputChangeAdd = event => {
+        // Create variables name and value from the event passed in as the parameter
+        // name wil be the data attribute, and the value will be it's value
+        const { value } = event.target;
+        // Setting the state to reflect the input changes
+        this.setState({
+            quantity: value
+        });
+    };
+
     render() {
         // Set variable to see if an image is in our state
         let isLoaded = false;
@@ -97,18 +137,21 @@ class Add_Cards extends Component {
                     handleFormSubmit={this.handleFormSubmit}
                     q={this.state.q}
                 />
-                <img src={this.state.imageUrl}></img>
-                <h1>{this.state.text}</h1>
+                {/* <img src={this.state.imageUrl}></img>
+                <h1>{this.state.text}</h1> */}
 
-                 {/* If an image is loaded, show component to add cards to our DB */}
-                 {/* {isLoaded ? (
+                {/* If an image is loaded, show component to add cards to our DB */}
+                {isLoaded ? (
                     <AddCards
-                        img={this.state.imageUrl}
+                        imageUrl={this.state.imageUrl}
+                        quantity={this.state.quantity}
+                        handleInputChangeAdd={this.handleInputChangeAdd}
+                        handleAddSubmit={this.handleAddSubmit}
                     />
-                // Otherwise load our message to user
+                    // Otherwise load our message to user
                 ) : (
                         <h1>{this.state.message}</h1>
-                    )} */}
+                    )}
             </Container>
         )
     }
