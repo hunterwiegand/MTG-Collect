3056451a2cb9238f5ca2c25812251a956d5449ef
Hunter Wiegand <hunter_wiegand@yahoo.com>
2020-03-21 20:09:37 -0700

    Placed name first on card schema

--- database/models/card.js
+++ database/models/card.js
@@ -5,10 +5,10 @@ mongoose.promise = Promise
 // Define cardSchema
 const cardSchema = new Schema({
 
+    name: { type: String, unique: true, required: true},
     quantity: { type: Number, unique: false, required: true},
     colors: { type: Array, unique: false, required: true},
     mana_cost: { type: String, unique: false, required: true},
-    name: { type: String, unique: true, required: true},
     type_line: { type: String, unique: false, required: true},
     rarity: { type: String, unique: false, required: true},
     oracle_text: { type: String, unique: false, required: true},
