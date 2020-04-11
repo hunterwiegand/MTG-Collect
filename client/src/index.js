e148ebd50ac7e3534f531d59d97c1c1672c997f2
Hunter Wiegand <hunter_wiegand@yahoo.com>
2020-04-10 19:14:42 -0700

    Added some bootstrap styles and worked on search parameters in collection

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+<link
+  rel="stylesheet"
+  href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+  integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+  crossorigin="anonymous"
+/>
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
